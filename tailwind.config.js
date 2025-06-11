b9ee77cc8b61b20f9eda480a3d493edd0452f396
KEVN <troostkevin8@gmail.com>
2025-06-11 15:54:47 +0200

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+export default {
+    content: [
+        "./src/app/navbar/navbar.component.html",
+        "./src/app/navbar/navbar.component.ts",
+        
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
